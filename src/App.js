b73281b730ae030f0916907aecbc52231b85bc60
iliav <iliav@hurdit.com>
2018-06-24 16:15:44 -0400

    Added reset.css, some graphic, almost done with wireframe's header

--- src/App.js
+++ src/App.js
@@ -1,37 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import '@progress/kendo-theme-bootstrap/dist/all.css';
-
-// Import the Button component.
 import { Button } from '@progress/kendo-react-buttons';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+import './reset.css';
+import './App.css';
+// import '@progress/kendo-theme-bootstrap/dist/all.css';
+import '@progress/kendo-theme-default/dist/all.css';
 
-    this.state = {
-      title: 'Welcome to React!'
-    }
-  }
-  onButtonClick = () => {
-    this.setState({title: 'Hello from Kendo UI!'})
-  }
+class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">{this.state.title}</h1>
+      <div className="app-root">
+        <header>
+          <div id="partner-logo">
+            {/* <img src="./img/partner-logo.png" /> */}
+            {/* <img src="./img/credible.png" /> */}
+            <img src="./img/sample-partner.jpg" alt="" />
+          </div>
+          <div id="header-content">
+            <div id="header-top">
+              <div id="partner-name">
+                <h1>Partner Name</h1>
+              </div>
+              <div id="logout-button">
+                <Button look="outline">Logout</Button>
+              </div>
+              <div id="help-button">
+                <i class="fas fa-question-circle fa-lg"></i>
+              </div>
+              <div id="service-logo">
+                <img src="./img/logo.png" alt="" />
+              </div>
+            </div>
+            <div id="header-bottom">
+              <div id="header-menu">Home | Client | Employee</div>
+              <div id="user-info">Logged in as: username 11:25 AM EST</div>
+            </div>
+          </div>
         </header>
-        <p className="App-intro">
-          <Button
-            onClick={this.onButtonClick}
-            primary={true}
-          >
-            My Kendo UI Button
-          </Button>
-        </p>
+        <nav>
+          <ul>
+            <li>Overview</li>
+            <li>1st Available</li>
+            <li>Amendments</li>
+            <li>Tx Plus</li>
+            <li>Attachments</li>
+            <li>Allergy</li>
+            <li>Visit List</li>
+            <li>Add Visit</li>
+            <li>Authorization</li>
+          </ul>
+        </nav>
+        <article>Content</article>
       </div>
     );
   }
